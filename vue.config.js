607b2ea05ee8902d29c8dd9ea05fe1cb24ecb37f
IrenaNguyen23 <nguyenbaohoa02@gmail.com>
2024-12-02 06:09:47 +0700

    Big Update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/' // Đảm bảo tài nguyên được load đúng từ root
 })
